6afb48ccbfdce45d73a0876f1f24f13cd9003d14
Jahongir <dodajoy0777@gmail.com>
2023-09-02 03:51:41 +0500

    qale

--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from .models import fullname, location
 from django.http import JsonResponse
+from .serializer import fullnameSerializer, locationSerializer
 
 # Create your views here.
 
 def all(request):
     all=fullname.objects.all()
-    mylist =[]
-    for i in all:
-        mylist.append({
-            'name':i.name,
-            'surname':i.surname,
-            'thirdname':i.thirdname
-        })
-    return JsonResponse(mylist, safe=False)
+    mylist =fullnameSerializer(all, many=True)
+    # for i in all:
+    #     mylist.append({
+    #         'name':i.name,
+    #         'surname':i.surname,
+    #         'thirdname':i.thirdname
+    #     })
+    return JsonResponse(mylist.data, safe=False)
     
 
 
 def detail(request, myid):
-    each = location.objects.get(id=myid)
-    each = get_object_or_404(location, id=myid)
-    data={'City name':each.city, 'Country name':each.country}
-    return JsonResponse(data)
+    # each = get_object_or_404(location, id=myid)
+    each = location.objects.filter(id=myid)
+    # data={'City name':each.city, 'Country name':each.country}
+    fornow=locationSerializer(each, many=True)
+    return JsonResponse(fornow.data, safe=False)
