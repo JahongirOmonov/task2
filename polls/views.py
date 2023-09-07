c0cf6f634cad6cedf3660eda8826bccb36f3e07c
Jahongir <dodajoy0777@gmail.com>
2023-09-07 07:22:00 +0500

    qale

--- polls/views.py
+++ polls/views.py
@@ -2,19 +2,28 @@ from django.shortcuts import render, get_object_or_404
 from .models import fullname, location
 from django.http import JsonResponse
 from .serializer import fullnameSerializer, locationSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 
-def all(request):
-    all=fullname.objects.all()
-    mylist =fullnameSerializer(all, many=True)
-    # for i in all:
-    #     mylist.append({
-    #         'name':i.name,
-    #         'surname':i.surname,
-    #         'thirdname':i.thirdname
-    #     })
-    return JsonResponse(mylist.data, safe=False)
+# def all(request):
+#     all=fullname.objects.all()
+#     mylist =fullnameSerializer(all, many=True)
+#     # for i in all:
+#     #     mylist.append({
+#     #         'name':i.name,
+#     #         'surname':i.surname,
+#     #         'thirdname':i.thirdname
+#     #     })
+#     return JsonResponse(mylist.data, safe=False)
+
+class getfullname(APIView):
+    def get(self, request):
+        bir = fullname.objects.all()
+        srr=fullnameSerializer(bir, many=True)
+        return Response(srr.data)
+
     
 
 
@@ -24,3 +33,13 @@ def detail(request, myid):
     # data={'City name':each.city, 'Country name':each.country}
     fornow=locationSerializer(each, many=True)
     return JsonResponse(fornow.data, safe=False)
+
+class postfullname(APIView):
+    def post(self, request):
+        main_body=request.data
+        sss = fullnameSerializer(data=main_body)
+        if sss.is_valid():
+            sss.save()
+            return Response(sss.data)
+        return Response(sss.errors)
+
